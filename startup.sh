5a64da6aa7c54bcb53a9b3b1255d83182bb06349
venkat <venkat@duplocloud.net>
2018-06-26 09:53:36 -0700

    https config to filebeat config

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+cat /opt/filebeat-5.4.3-linux-x86_64/filebeat.yml
+sed -i 's/<HOSTNAME>/'$HOSTNAME'/' /opt/filebeat-5.4.3-linux-x86_64/filebeat.yml
 sed -i 's/<HOSTS>/'$HOSTS'/' /opt/filebeat-5.4.3-linux-x86_64/filebeat.yml
+cat /opt/filebeat-5.4.3-linux-x86_64/filebeat.yml
 /bin/tini -- /bin/executor.sh
 
