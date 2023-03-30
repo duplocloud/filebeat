bdd6af43adf66223fed63640d75261dbffb36927
Srikar Naramsetty <srikar@duplocloud.net>
2023-03-30 13:57:37 +0530

    multi arch filebeat

---
+++ diagnostics/opensearch/filebeat/buildAndPush.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+revision=$1
+echo $revision
+
+docker buildx build --push --platform linux/arm64,linux/amd64 --tag duplocloud/filebeat-oss:7.11.1-${revision} .
+
+
+docker buildx build --push --platform linux/arm64,linux/amd64 --tag duplocloud/filebeat-oss:7.11.1-${revision}-k8 --file Dockerfile-k8s .
