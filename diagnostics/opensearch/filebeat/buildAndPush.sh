11298f50fabbb8e2b7a22c68c740be9e724a29af
Yashwant Bokadia <yashwant@duplocloud.net>
2024-02-14 11:14:32 +0530

    custom filebeat changes
    custom filebeat changes

--- diagnostics/opensearch/filebeat/buildAndPush.sh
+++ diagnostics/opensearch/filebeat/buildAndPush.sh
@@ -4,5 +4,6 @@ echo $revision
 
 docker buildx build --push --platform linux/arm64,linux/amd64 --tag duplocloud/filebeat-oss:7.11.1-${revision} .
 
-
 docker buildx build --push --platform linux/arm64,linux/amd64 --tag duplocloud/filebeat-oss:7.11.1-${revision}-k8 --file Dockerfile-k8s .
+
+docker buildx build --push --platform linux/arm64,linux/amd64 --tag duplocloud/filebeat-oss:7.11.1-${revision}-custom --file Dockerfile-custom .
